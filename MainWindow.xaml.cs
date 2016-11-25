49fadfdb8c5791677e8b28b1dd1bf2d57e3dfc27
whhub <wanghui122004@me.com>
2016-11-25 17:52:32 +0800

    WPF Solution Initialize

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace XamlAttribute
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
