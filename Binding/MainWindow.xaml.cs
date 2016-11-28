acf26e0f47dc4b7420e568b0142d0ba77e81a630
whhub <wanghui122004@me.com>
2016-11-28 13:49:40 +0800

    [WPF][Chapter06][Binding] ItemsSource ItemTemplate

--- Binding/MainWindow.xaml.cs
+++ Binding/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 
@@ -20,6 +21,20 @@ namespace Binding
             binding.Path = new PropertyPath("Name");
 
             BindingOperations.SetBinding(TextBox, TextBox.TextProperty, binding);
+
+
+            var stuList = new List<Student>
+            {
+                new Student {Id = 0, Name = "Time", Age = 29},
+                new Student {Id = 2, Name = "Tom", Age = 28},
+                new Student {Id = 4, Name = "Vina", Age = 27}
+            };
+
+            ListBoxStudents.ItemsSource = stuList;
+            //ListBoxStudents.DisplayMemberPath = "Name";
+
+            var binding2 = new System.Windows.Data.Binding("SelectedItem.Id") {Source = ListBoxStudents};
+            TextBoxID.SetBinding(TextBox.TextProperty, binding2);
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
