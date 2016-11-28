77daba6cbe4f0d306f10edc92a00f4154a754905
whhub <wanghui122004@me.com>
2016-11-28 21:56:47 +0800

    [WPF][Chapter03]XamlAttribute

---
+++ XamlAttribute/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace XamlAttribute
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ XamlAttribute/Human.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XamlAttribute
+{
+    [TypeConverter(typeof(StringToHumanTypeConverter))]
+    class Human
+    {
+        public string Name { get; set; }
+        public Human Child { get; set; }
+    }
+}
