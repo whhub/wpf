4048450318712847f2f8eec428c116f672654185
whhub <wanghui122004@me.com>
2016-12-10 10:42:25 +0800

    [WPF] Command Concept （in backend code）

--- Commands/MainWindow.xaml.cs
+++ Commands/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Commands
+﻿using System.Windows.Input;
+
+namespace Commands
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
@@ -8,6 +10,20 @@
         public MainWindow()
         {
             InitializeComponent();
+            InitializeCommand();
+        }
+
+        // 声明并定义命令
+        private RoutedCommand clearCmd = new RoutedCommand("Clear", typeof(MainWindow));
+
+        private void InitializeCommand()
+        {
+            // 把命令赋值给命令源（发送者）并指定快捷键
+            Button1.Command = clearCmd;
+            clearCmd.InputGestures.Add(new KeyGesture(Key.C, ModifierKeys.Alt));
+
+            // 指定命令目标
+            Button1.CommandTarget = TextBoxA;
         }
     }
 }
