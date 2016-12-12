0571064d45716bac465dbbc79f52b447f94adf39
whhub <wanghui122004@me.com>
2016-12-12 14:10:23 +0800

    [WPF][Chapter09][01] Command Concept (in backend code)  command / source / target / binding / canExecute / Executed

--- Commands/MainWindow.xaml.cs
+++ Commands/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 
 namespace Commands
 {
@@ -14,16 +15,43 @@ namespace Commands
         }
 
         // 声明并定义命令
-        private RoutedCommand clearCmd = new RoutedCommand("Clear", typeof(MainWindow));
+        private readonly RoutedCommand _clearCmd = new RoutedCommand("Clear", typeof(MainWindow));
 
         private void InitializeCommand()
         {
             // 把命令赋值给命令源（发送者）并指定快捷键
-            Button1.Command = clearCmd;
-            clearCmd.InputGestures.Add(new KeyGesture(Key.C, ModifierKeys.Alt));
+            Button1.Command = _clearCmd;
+            _clearCmd.InputGestures.Add(new KeyGesture(Key.C, ModifierKeys.Alt));
 
             // 指定命令目标
             Button1.CommandTarget = TextBoxA;
+
+            // 创建命令关联u
+            var cb = new CommandBinding();
+            cb.Command = _clearCmd;  // 只关注与_clearCmd相关的事件
+            cb.CanExecute += CbOnCanExecute;
+            cb.Executed += CbOnExecuted;
+
+            // 把命令关联安置在外围控件上
+            StackPanel.CommandBindings.Add(cb);
+        }
+
+        // 当命令送达目标后，此方法被调用
+        private void CbOnExecuted(object sender, ExecutedRoutedEventArgs executedRoutedEventArgs)
+        {
+            TextBoxA.Clear();
+
+            // 避免继续向上传而降低性能
+            executedRoutedEventArgs.Handled = true;
+        }
+
+        // 当探测命令是否可以执行时，此方法被调用
+        private void CbOnCanExecute(object sender, CanExecuteRoutedEventArgs canExecuteRoutedEventArgs)
+        {
+            canExecuteRoutedEventArgs.CanExecute = !string.IsNullOrEmpty(TextBoxA.Text);
+
+            // 避免继续向上传而降低程序性能
+            canExecuteRoutedEventArgs.Handled = true;
         }
     }
 }
